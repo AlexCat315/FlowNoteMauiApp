e8d224862dd8afee47a01f008ea1aff2e9e8e9f9
chenyike <cyke0315@163.com>
2026-02-25 12:36:23 +0800

    initialization

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ExampleMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		LanguageManager.Initialize();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
